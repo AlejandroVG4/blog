dfaa61e1668bc8c3bc2d14e3246ec52e06c34789
AlejandroVG4 <Vanegasgarcia1104@gmail.com>
2025-02-27 14:29:25 -0500

    Configurando settings

--- blog/settings.py
+++ blog/settings.py
@@ -39,7 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     #drf
-    'rest_framework'
+    'rest_framework',
+    
+    #app
+    'usuarios'
 ]
 
 MIDDLEWARE = [
--- usuarios/models.py
+++ usuarios/models.py
@@ -7,7 +7,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Etiqueta(models.Model):
-    nombre = models.CharField(unique=True)
+    nombre = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.nombre
