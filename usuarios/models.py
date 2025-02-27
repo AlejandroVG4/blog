ec8b08e35eb260f4ee323226c6f0e3b1fdd528fa
AlejandroVG4 <Vanegasgarcia1104@gmail.com>
2025-02-27 14:10:48 -0500

    Subiendo proyecto

---
+++ usuarios/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+# from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Etiqueta(models.Model):
+    nombre = models.CharField(unique=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Publicacion(models.Model):
+    fecha_hora = models.DateTimeField(auto_now_add=True)
+    usuario_id = models.ForeignKey(User, on_delete=models.CASCADE)  
+    texto = models.TextField()
+    etiqueta = models.ForeignKey(Etiqueta, on_delete=models.CASCADE)  
+
+    def __str__(self):
+        return f"Publicación de {self.usuario.username} - {self.texto[:20]}..." 
+
+class Comentario(models.Model):
+    fecha_hora = models.DateTimeField(auto_now_add=True)
+    texto = models.TextField()
+    publicacion_id = models.ForeignKey(Publicacion, on_delete=models.CASCADE)
+    usuario_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
