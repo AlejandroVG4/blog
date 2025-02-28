6d7028cb486e430664fd1f7120b288beb95a2ff3
Yulissa <lyulissa5@gmail.com>
2025-02-27 22:54:18 -0500

    updating user urls

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -5,8 +5,8 @@ from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('registro/', views.RegistroView.as_view(), name='registro'),
-    path('api/token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('token/', views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('perfil/', views.PerfilView.as_view(), name='perfil'),
     path('eliminar/', views.EliminarUsuarioView.as_view(), name='eliminar_usuario'),
     path('actualizar/', views.ActualizarPerfilView.as_view(), name='actualizar'),
